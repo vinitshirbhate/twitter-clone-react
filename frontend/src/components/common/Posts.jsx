2c92914d256f7ffb3b6bb685ed570832b3d2677f
Vinit Shirbhate <vinit.shirbhate23@vit.edu>
2024-06-30 23:54:55 +0530

    deleting and fetching posts

--- frontend/src/components/common/Posts.jsx
+++ frontend/src/components/common/Posts.jsx
@@ -1,25 +1,65 @@
 import Post from "./Post";
 import PostSkeleton from "../skeletons/PostSkeleton";
-import { POSTS } from "../../utils/db/dummy";
+import { useQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
 
-const Posts = () => {
-  const isLoading = false;
+const Posts = ({ feedtype }) => {
+  const getPosts = () => {
+    switch (feedtype) {
+      case "forYou":
+        return "/api/post/all";
+      case "following":
+        return "/api/post/following";
+      default:
+        return "/api/post/all";
+    }
+  };
+
+  const getPostsData = getPosts();
+
+  const {
+    data: Posts,
+    isLoading,
+    refetch,
+    isRefetching,
+  } = useQuery({
+    queryKey: ["posts"],
+    queryFn: async () => {
+      try {
+        const res = await fetch(getPostsData);
+        const data = await res.json();
+
+        if (!res.ok) {
+          throw new Error(data.error || "Something went wrong");
+        }
+
+        return data;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+  });
+
+  useEffect(() => {
+    refetch();
+  }, [feedtype, refetch]);
 
   return (
     <>
-      {isLoading && (
-        <div className="flex flex-col justify-center">
-          <PostSkeleton />
-          <PostSkeleton />
-          <PostSkeleton />
-        </div>
-      )}
-      {!isLoading && POSTS?.length === 0 && (
+      {isLoading ||
+        (isRefetching && (
+          <div className="flex flex-col justify-center">
+            <PostSkeleton />
+            <PostSkeleton />
+            <PostSkeleton />
+          </div>
+        ))}
+      {!isLoading && !isRefetching && Posts?.length === 0 && (
         <p className="text-center my-4">No posts in this tab. Switch 👻</p>
       )}
-      {!isLoading && POSTS && (
+      {!isLoading && !isRefetching && Posts && (
         <div>
-          {POSTS.map((post) => (
+          {Posts.map((post) => (
             <Post key={post._id} post={post} />
           ))}
         </div>
